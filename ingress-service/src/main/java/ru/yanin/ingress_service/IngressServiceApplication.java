8d6bd16606b5eaf15d42736c1e3f379bd7e776d1
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2026-07-02 11:50:42 +0300

    feat: add frame of ingress-service

---
+++ ingress-service/src/main/java/ru/yanin/ingress_service/IngressServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.yanin.ingress_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngressServiceApplication.class, args);
+    }
+
+}
