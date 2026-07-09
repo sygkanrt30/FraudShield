1d809a2931d8475b28a14aa3f03ffc3742f174f7
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2026-07-09 11:03:39 +0300

    feat: add skeleton of graph-ingress-service

---
+++ graph-ingress-service/src/main/java/ru/yanin/graph_ingress/GraphIngressServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.yanin.graph_ingress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphIngressServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphIngressServiceApplication.class, args);
+    }
+
+}
