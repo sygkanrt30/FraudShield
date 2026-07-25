f5b1e968a160ced881b900f208e63551763bbe0b
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2026-07-25 12:46:02 +0300

    feat: add raw-transactions-dlq, in case kafka-connect won't be able to save transaction

--- clickhouse/kafka-connect-setup.sh
+++ clickhouse/kafka-connect-setup.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
-
 set -e
 
 echo "Installing ClickHouse Kafka Connector..."
 confluent-hub install --no-prompt clickhouse/clickhouse-kafka-connect:latest
 
 echo "Creating ClickHouse sink connector..."
-curl -X POST http://localhost:8083/connectors \
+curl -X PUT http://localhost:8083/connectors/clickhouse-sink-raw-transactions/config \
   -H "Content-Type: application/json" \
   -d '{
-    "name": "clickhouse-sink-raw-transactions",
-    "config": {
-      "connector.class": "com.clickhouse.kafka.connect.ClickHouseSinkConnector",
-      "tasks.max": "1",
-      "topics": "raw-transactions",
-      "clickhouse.server.url": "clickhouse:8123",
-      "clickhouse.username": "kafka_connect",
-      "clickhouse.password": "kafka_connect_pass",
-      "clickhouse.database": "transactions",
-      "clickhouse.table": "raw_transactions",
-      "clickhouse.errors.tolerance": "all",
-      "clickhouse.ignore.errors": "true",
-      "key.converter": "org.apache.kafka.connect.storage.StringConverter",
-      "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-      "value.converter.schemas.enable": "false",
-      "clickhouse.batch.size": "1000",
-      "clickhouse.batch.interval": "1000",
-      "clickhouse.retry.on.error": "true",
-      "clickhouse.retry.max.attempts": "3"
-    }
+    "connector.class": "com.clickhouse.kafka.connect.ClickHouseSinkConnector",
+    "tasks.max": "1",
+    "topics": "raw-transactions",
+
+    "clickhouse.server.url": "clickhouse:8123",
+    "clickhouse.username": "kafka_connect",
+    "clickhouse.password": "kafka_connect_pass",
+    "clickhouse.database": "transactions",
+    "clickhouse.table": "transactions",
+
+    "key.converter": "org.apache.kafka.connect.storage.StringConverter",
+    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+    "value.converter.schemas.enable": "false",
+
+    "clickhouse.batch.size": "1000",
+    "clickhouse.batch.interval": "1000",
+    "clickhouse.retry.on.error": "true",
+    "clickhouse.retry.max.attempts": "5",
+
+    "errors.tolerance": "all",
+    "errors.log.enable": "true",
+    "errors.log.include.messages": "true",
+    "errors.deadletterqueue.topic.name": "raw-transactions-dlq",
+    "errors.deadletterqueue.topic.replication.factor": 1,
+    "errors.deadletterqueue.context.headers.enable": "true"
   }'
 
 echo "Kafka Connect setup finished!"
