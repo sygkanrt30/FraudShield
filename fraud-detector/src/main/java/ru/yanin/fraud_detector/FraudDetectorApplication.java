ac50f48af330d15e74bc8aa907f9725d8d7195ad
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2026-07-25 11:35:52 +0300

    feat: add fraud-detector module

---
+++ fraud-detector/src/main/java/ru/yanin/fraud_detector/FraudDetectorApplication.java
@@ -0,0 +1,13 @@
+package ru.yanin.fraud_detector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudDetectorApplication.class, args);
+    }
+
+}
