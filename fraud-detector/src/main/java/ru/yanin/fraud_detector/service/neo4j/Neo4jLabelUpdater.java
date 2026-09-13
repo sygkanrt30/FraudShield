3ac0acc6894018b35f7c0e29176dc9cea43bc76a
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2026-09-13 11:20:07 +0300

    feat: implement Neo4jLabelUpdater

--- fraud-detector/src/main/java/ru/yanin/fraud_detector/service/neo4j/Neo4jLabelUpdater.java
+++ fraud-detector/src/main/java/ru/yanin/fraud_detector/service/neo4j/Neo4jLabelUpdater.java
@@ -1,11 +1,14 @@
 package ru.yanin.fraud_detector.service.neo4j;
 
 import ru.yanin.fraud_detector.dto.RiskScores;
+import ru.yanin.fraud_detector.model.clickhouse.FraudMetrics;
+
+import java.util.UUID;
 
 /**
  * @author Vyacheslav Yanin
  */
 public interface Neo4jLabelUpdater {
 
-    void updateLabels(RiskScores riskScores);
+    void updateLabels(UUID clientId, RiskScores riskScores, FraudMetrics fraudMetrics);
 }
--- fraud-detector/src/main/java/ru/yanin/fraud_detector/service/neo4j/Neo4jLabelUpdaterImpl.java
+++ fraud-detector/src/main/java/ru/yanin/fraud_detector/service/neo4j/Neo4jLabelUpdaterImpl.java
@@ -3,7 +3,15 @@ package ru.yanin.fraud_detector.service.neo4j;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.yanin.fraud_detector.dto.RiskScores;
+import ru.yanin.fraud_detector.exception.ClientNotFoundException;
+import ru.yanin.fraud_detector.model.clickhouse.FraudMetrics;
+import ru.yanin.fraud_detector.model.neo4j.Client;
+import ru.yanin.fraud_detector.repo.neo4j.ClientNeo4jRepository;
+
+import java.math.BigDecimal;
+import java.util.UUID;
 
 /**
  * @author Vyacheslav Yanin
@@ -13,8 +21,27 @@ import ru.yanin.fraud_detector.dto.RiskScores;
 @RequiredArgsConstructor
 public class Neo4jLabelUpdaterImpl implements Neo4jLabelUpdater {
 
-    @Override
-    public void updateLabels(RiskScores riskScores) {
+    private final ClientNeo4jRepository neo4jRepository;
 
+    @Override
+    @Transactional
+    public void updateLabels(UUID clientId, RiskScores riskScores, FraudMetrics fraudMetrics) {
+        Client client = neo4jRepository.findClientByClientId(clientId).orElseThrow(ClientNotFoundException::new);
+        updateClient(client, riskScores, fraudMetrics);
+        neo4jRepository.save(client);
+        log.debug("Client's ({}) labels have been successfully updated", clientId);
+    }
+
+    private void updateClient(Client client, RiskScores riskScores, FraudMetrics fraudMetrics) {
+        client.setSuspicious(riskScores.overallRisk() > 0.6);
+        client.setVictim(riskScores.victimRisk() > 0.7);
+
+        client.setFraudsterRisk(new BigDecimal(riskScores.fraudsterRisk()));
+        client.setVictimRisk(new BigDecimal(riskScores.victimRisk()));
+        client.setOverallRisk(new BigDecimal(riskScores.overallRisk()));
+
+        client.setTotalSentToHubs(fraudMetrics.totalSentToHubs());
+        client.setTxCountToHubs(fraudMetrics.txCountToHubs());
+        client.setLastCheckedAt(fraudMetrics.calculatedAt());
     }
 }
